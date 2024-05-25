fdbba15213561623267e884724b9e2606c7f0474
sgerodes <sgerodes@gmail.com>
2024-05-25 17:24:38 +0200

    feat: adds public key read

--- play.py
+++ play.py
@@ -1,22 +1,25 @@
-from ecies import encrypt, decrypt
+import os
+import dotenv
 from eth_keys import keys
-from eth_utils import decode_hex
+from eth_utils import decode_hex, encode_hex, keccak
+from ecies import encrypt, decrypt
+
+dotenv.load_dotenv()
 
 # Your Ethereum private key (hex format)
-private_key_hex = '0xf192a8df09cd3219a0f5b16020e3a3310be1625a0a4152599a1fa0ff5f7ed6bc'
+private_key_hex = os.getenv('PRIVATE_KEY')
 private_key = keys.PrivateKey(decode_hex(private_key_hex))
-
-# Corresponding public key
 public_key = private_key.public_key
+eth_address = public_key.to_checksum_address()
+message = b"Your secret message"
+
+print(f'public_key {private_key.public_key}')
+print(f'Ethereum address: {eth_address}')
 
 # Public key in uncompressed format (65 bytes)
 public_key_bytes = b'\x04' + public_key.to_bytes()
-
-# Encrypt message with public key
-message = b"Your secret message"
 encrypted_message = encrypt(public_key_bytes, message)
 
-# Decrypt message with private key
 decrypted_message = decrypt(private_key.to_bytes(), encrypted_message)
 
 assert message == decrypted_message
